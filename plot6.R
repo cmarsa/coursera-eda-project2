fe3bf0f200972544eab1f30e9d900c92fcf9b4a6
cmarsa <marcodelossantos@protonmail.com>
2021-08-18 17:46:22 -0500

    Changes to `plot6.R`.

--- plot6.R
+++ plot6.R
@@ -1,4 +1,6 @@
 # plot6.R
+library(dplyr)
+library(ggplot2)
 
 NEI <- readRDS("data/exdata_data_NEI_data/summarySCC_PM25.rds")
 SCC <- readRDS("data/exdata_data_NEI_data/Source_Classification_Code.rds")
@@ -10,12 +12,14 @@ aggregatedTotalByYearAndFips <- aggregate(Emissions ~ year + fips, subsetNEI, su
 aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="24510"] <- "Baltimore, MD"
 aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="06037"] <- "Los Angeles, CA"
 
-png("plot6.png", width=1040, height=480)
-g <- ggplot(aggregatedTotalByYearAndFips, aes(factor(year), Emissions))
-g <- g + facet_grid(. ~ fips)
-g <- g + geom_bar(stat="identity")  +
+png("plot6.png", width=800, height=480, units='px')
+g <- aggregatedTotalByYearAndFips |> 
+    ggplot(aes(factor(year), Emissions)) +
+    facet_grid(. ~ fips) +
+    geom_bar(stat="identity")  +
     xlab("year") +
     ylab(expression('Total PM'[2.5]*" Emissions")) +
     ggtitle('Total Emissions from motor vehicle (type=ON-ROAD) in Baltimore City, MD (fips = "24510") vs Los Angeles, CA (fips = "06037")  1999-2008')
+
 print(g)
 dev.off()
