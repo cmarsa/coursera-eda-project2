a008470176cea4f93c48717f5513ed8da1b956c5
cmarsa <marcodelossantos@protonmail.com>
2021-08-18 17:01:20 -0500

    Changing units of dimension of graphics to `px`.

--- plot1.R
+++ plot1.R
@@ -7,10 +7,10 @@ SCC <- readRDS("data/exdata_data_NEI_data/Source_Classification_Code.rds")
 year_emmisions <- aggregate(Emissions ~ year, NEI, sum)
 
 # plot emmisions as barplot
-png('plot1.png')
+png("plot1.png",width=480,height=480,units="px")
 barplot(height=year_emmisions$Emissions,
         names.arg=year_emmisions$year,
-        xlab="years",
+        xlab="Year",
         ylab=expression('Total PM_2.5 emission'),
         main=expression('Total PM_2.5 emissions by year'))
 dev.off()
